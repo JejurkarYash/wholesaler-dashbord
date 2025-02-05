d62d6255eeb9139aab71fe6bea1529269f84c383
Yash Jejurkar <yashjejurkar2004@gmail.com>
2025-02-05 23:53:30 +0530

    Redesign:mongo-schema

--- Invoice-backend/src/db/db.js
+++ Invoice-backend/src/db/db.js
@@ -1,8 +1,14 @@
 import mongoose, { model, Schema, Types } from "mongoose";
 
-// in the below all schema's id will be generated automatically !
+//
+const vendorSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  address: { type: String, required: true },
+  contactNumber: { type: String, required: true },
+});
 const customerSchema = new Schema({
-  // customerId: { type: mongoose.Types.ObjectId, auto: true },
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
@@ -11,39 +17,96 @@ const customerSchema = new Schema({
 });
 
 const productSChema = new Schema({
-  // productId: { type: mongoose.Types.ObjectId, auto: true },
   name: { type: String, required: true },
   price: { type: Number, required: true },
   stock: { type: Number, required: true },
+  catogory: { type: String },
 });
 
-const productVariantSchema = new Schema({
-  serialNumber: { type: String, required: true, unique: true },
+const allProductDetilsSchema = new Schema({
+  serialNumber: { type: Number, required: true, unique: true },
   productId: { type: mongoose.Types.ObjectId, ref: "Products", required: true },
   color: String,
   size: String,
   price: { type: String, required: true },
 });
 
-const orderSchema = new Schema({
-  // id will generate
-  // orderId: { type: mongoose.Types.ObjectId, auto: true },
-  customerId: { type: mongoose.Types.ObjectId, ref: "Customers" },
-  orderDetail: [
+const vendorOrderSchema = new Schema({
+  vendorId: { type: mongoose.Types.ObjectId, ref: "Vendors" },
+  orders: [
     {
+      orderId: { type: mongoose.Types.ObjectId, auto: true },
       productId: { type: mongoose.Types.ObjectId, ref: "Products" },
       totalQuantity: { type: Number, required: true },
       deliverdQuantity: { type: Number, default: 0 },
       pendingQuantity: { type: Number, required: true },
+      orderTotalPrice: { type: Number, require: true },
+      orderDate: { type: Date, default: Date.now },
+      productSerialNumbers: [
+        {
+          serialNumber: { type: Number, require: true, uniqu: true },
+        },
+      ],
     },
   ],
-  totalPrice: { type: Number, required: true },
-  orderDate: { type: Date, default: Date.now },
+});
+const customerOrderSchema = new Schema({
+  customerId: { type: mongoose.Types.ObjectId, ref: "Customers" },
+  orders: [
+    {
+      orderId: { type: mongoose.Types.ObjectId, auto: true },
+      productId: { type: mongoose.Types.ObjectId, ref: "Products" },
+      totalQuantity: { type: Number, required: true },
+      deliverdQuantity: { type: Number, default: 0 },
+      pendingQuantity: { type: Number, required: true },
+      orderTotalPrice: { type: Number, require: true },
+      orderDate: { type: Date, default: Date.now },
+      productSerialNumbers: [
+        {
+          serialNumber: { type: Number, require: true, uniqu: true },
+        },
+      ],
+    },
+  ],
 });
 
-const invoiceSchema = new Schema({
-  invoiceId: { type: mongoose.Types.ObjectId, auto: true },
-  orderId: { type: mongoose.Types.ObjectId, ref: "Orders", require: true },
+const vendorInvoiceSchema = new Schema({
+  orderId: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "VenderOrders",
+      require: true,
+    },
+  ],
+  vendorId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Vendors",
+    require: true,
+  },
+  products: [
+    {
+      productId: {
+        type: mongoose.Types.ObjectId,
+        ref: "Products",
+        require: true,
+      },
+      quantity: { type: String, require: true },
+      price: { type: Number, require: true },
+      total: { type: Number, require: true },
+    },
+  ],
+  invoiceDate: { type: Date, default: Date.now },
+  totalAmount: { type: Number, require: true },
+  status: { type: String, enum: ["pending", "paid"], default: "pending" },
+});
+const customerInvoiceSchema = new Schema({
+  orderId: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "VenderOrders",
+      require: true,
+    },
+  ],
   customerId: {
     type: mongoose.Types.ObjectId,
     ref: "Customers",
@@ -68,10 +131,22 @@ const invoiceSchema = new Schema({
 
 // exporting all schema's
 export const CustomerModel = new model("Customers", customerSchema);
+export const VendorModel = new model("Vendors", vendorSchema);
 export const ProductModel = new model("Products", productSChema);
-export const ProductVariantModel = new model(
-  "ProductVariant",
-  productVariantSchema
+export const AllProductDetilsModel = new model(
+  "AllProductDetails",
+  allProductDetilsSchema
 );
-export const OrderModel = new model("Orders", orderSchema);
-export const InvoiceModel = new model("Invoice", invoiceSchema);
+export const VendorOrdersModel = new model("VendorOrders", vendorOrderSchema);
+export const CustomerOrdersModel = new model(
+  "CustomerOrders",
+  customerOrderSchema
+);
+export const VendorInvoiceModel = new model(
+  "VendorsInvoice",
+  vendorInvoiceSchema
+);
+export const CustomerInvoiceModel = new model(
+  "VendorsInvoice",
+  customerInvoiceSchema
+);
